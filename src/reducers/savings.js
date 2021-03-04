46fb0066858e5500ef68abc15360b341352a2910
Timothy Yee <mingfeezy@gmail.com>
2021-03-03 17:30:46 -0800

    added savings component

--- src/reducers/savings.js
+++ src/reducers/savings.js
@@ -1,5 +1,9 @@
+import * as ActionTypes from '../actions/ActionTypes';
+
 const SavingsReducer = (state = [], action) => {
     switch(action.type) {
+        case ActionTypes.ADD_SAVINGS:
+            return {...state, savings: action.payload}
         default:
             return state;
     }
